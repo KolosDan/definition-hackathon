3f13fb75ef5014ce4bebbfdc5a87060b69742432
KolosDan <dan.danya99@gmail.com>
2021-09-26 14:53:40 +0300

    .

--- frontend/src/components/Mint.vue
+++ frontend/src/components/Mint.vue
@@ -177,7 +177,7 @@ export default {
           signer: toAddress(this.accounts[0])
         }
 
-        const lres = await fetch(`https://api-ropsten.rarible.com/marketplace/api/v4/items/${res.itemId}/lock`, {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(lockBody)}).then((r) => r.json())
+        const lres = await fetch(`https://api-ropsten.rarible.com/marketplace/api/v4/items/${res.itemId}/lock`, {credentials: "include", method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify(lockBody)}).then((r) => r.json())
         console.log(lres)
     }
   },
